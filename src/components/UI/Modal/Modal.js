1dacc060cd179efe7c99b08a8e07c8289992b8cf
vishal <vishal.joshi.021@gmail.com>
2018-08-29 18:13:29 -0400

    adding button component

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import classes from './Modal.css';
+import Aux from '../../../hoc/Auxiliary';
+import BackDrop from '../BackDrop/BackDrop';
 
 const modal = (props) => (
-    <div
-        style={{
-            transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-            opacity: props.show ? '1': '0'
-        }} 
-        className={classes.Modal}>
-        {props.children}
-    </div>
+    <Aux>
+        <BackDrop show={props.show} clicked={props.modalClosed}/>
+        <div
+            style={{
+                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                opacity: props.show ? '1': '0'
+            }} 
+            className={classes.Modal}>
+            {props.children}
+        </div>
+    </Aux>
 );
 
 export default modal;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -75,6 +75,14 @@ class BurgerBuilder extends Component {
         this.setState({purchasing: true});
     }
 
+    purchaseCancelHandler = () => {
+        this.setState({purchasing: false});
+    }
+
+    purchaseContinueHandler = () => {
+        alert('Purchased!');
+    }
+
     render() {
         const disabledInfo = {
             ...this.state.ingredients
@@ -86,8 +94,11 @@ class BurgerBuilder extends Component {
 
         return (
             <Aux>
-                <Modal show={this.state.purchasing}>
-                    <OrderSummary ingredients={this.state.ingredients}/>
+                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
+                    <OrderSummary 
+                        ingredients={this.state.ingredients}
+                        purchaseCancel={this.purchaseCancelHandler}
+                        purchaseContinue={this.purchaseContinueHandler}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls 
