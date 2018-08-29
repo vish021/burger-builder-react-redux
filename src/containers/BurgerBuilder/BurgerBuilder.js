5ff9b9243e35d17f11774be8cd0e4bb4b050e7fa
vishal <vishal.joshi.021@gmail.com>
2018-08-29 15:26:42 -0400

    adding total price and rmeove handler

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -39,16 +39,37 @@ class BurgerBuilder extends Component {
     }
 
     removeIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        if(oldCount <= 0) {
+            return;
+        }
+        const updatedCount = oldCount - 1;
+        const updatedIngredients = {//state objects are immutable
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const newTotalPrice = this.state.totalPrice - INGREDIENT_PRICES[type];
 
+        this.setState({totalPrice: newTotalPrice, ingredients: updatedIngredients});
     }
 
     render() {
+        const disabledInfo = {
+            ...this.state.ingredients
+        };
+
+        for (let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0;
+        }
+
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls 
                     ingredientAdded={this.addIngredientHandler}
-                />
+                    ingredientRemoved={this.removeIngredientHandler}
+                    disabled={disabledInfo}
+                    price={this.state.totalPrice}/>
             </Aux>
         );
     }
