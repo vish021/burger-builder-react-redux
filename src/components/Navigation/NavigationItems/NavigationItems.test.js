bf8ade3bf2b8886bc559bbc9fcdaaf5e81d58805
vish021 <vishal.joshi.021@gmail.com>
2018-09-19 13:36:28 -0400

    using enzyme method contains for test cases

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -7,8 +7,24 @@ import NavigationItem from './NavigationItem/NavigationItem';
 configure({adapter: new Adapter()});
 
 describe('<NavigationItems />', () => {
+    //Executes before each test
+    let wrapper;
+    beforeEach(() => {
+        wrapper =  shallow(<NavigationItems />);//JSX code 
+    });
+
     it('should render two <NavigationItem /> elements if not authenticated', () => {
-        const wrapper = shallow(<NavigationItems />);//JSX code 
         expect(wrapper.find(NavigationItem)).toHaveLength(2);//not JSX but just a function or a class
+    });
+
+    it('should render three <NavigationItem /> elements if authenticated', () => {
+        //wrapper = shallow(<NavigationItems isAuthenticated/>);
+        wrapper.setProps({isAuthenticated: true});// set props before running test
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);//not JSX but just a function or a class
+    });
+
+    it('should render Logout elements if authenticated', () => {
+        wrapper.setProps({isAuthenticated: true});// set props before running test
+        expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
     });
 });
